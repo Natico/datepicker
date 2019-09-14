dd52677ca467193789eeb5d14ccf55ea6be09a7b
natico <natamjadian.n@gmail.com>
2019-09-14 21:34:35 +0430

    first commit

---
+++ src/components/DatePickerInput.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Input } from 'semantic-ui-react';
+
+class DatePickerInput extends React.Component {
+  render() {
+    <Input action='Search' placeholder='Search...' />;
+  }
+}
+
+export default DatePickerInput;
---
+++ src/components/DatePickerInputIcon.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Icon } from 'semantic-ui-react';
+
+class DatePickerInputIcon extends React.Component {
+  render() {
+    <Icon disabled name='users' />;
+  }
+}
+
+export default DatePickerInputIcon;
